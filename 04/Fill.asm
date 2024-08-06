01c30d54d8394f119825c276e6e4e634f92d1a50
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-08-06 17:31:22 -0500

    filled

--- 04/Fill.asm
+++ 04/Fill.asm
@@ -1,42 +1,73 @@
-// get last position that we want to fill until
+// fillUntil last word in screen
 @KBD
-D=M
+D=A
 @fillUntil
 M=D-1
+
 // init numFilled
 @numFilled
 M=0
+
 (LOOP)
-    // jump to draw if a key has been pressed  
+    // draw if there is a keypress
     @KBD
-    D=M 
-    @DRAW
+    D=M
+    @DRAW      
     D;JGT
-    // if there is something in screen and we are not pressing kbd
-    // @SCREEN
-    // D=M
-    // @CLEAR
-    // D;JGT
-    @LOOP
-    0;JMP
-(DRAW)
-    // check if we have everything filled (meaning n = fillUntil)
-    @numFilled 
+
+(DRAW)  
+    // if no keypress, clear
+    @KBD
+    D=M
+    @CLEAR
+    D;JEQ
+
+    @numFilled
     D=M
     @fillUntil
     D=D-M
-    // if full, go back to loop?
-    @LOOP
+    @REDRAW
     D;JEQ
-    // if not full, then fill up the grid...
+    // put screen start in D
     @SCREEN
-    D=M
+    D=A
     @numFilled
     A=D+M
     M=-1
     @numFilled
-    D=M+1
+    M=M+1
+    @LOOP
+    0;JMP
+
+(REDRAW)
+    @numFilled
+    D=0
     @DRAW
     0;JMP
-//(CLEAR)
-    // honestly do the same thing but change all the pixels to white (up until the number we have filled)
+
+(CLEAR)
+    // if numFilled is 0, jump to clear root (and then back to main loop)
+    @numFilled
+    D=M
+    @CLEAR_ROOT
+    D;JEQ
+
+    // else we have some clearing to do
+    @SCREEN
+    D=A
+    @numFilled
+    A=D+M
+    M=0
+    @numFilled
+    M=M-1
+    @CLEAR
+    0;JMP
+
+(CLEAR_ROOT)
+    @SCREEN
+    M=0
+    @LOOP
+    0;JMP
+
+(INF)
+    0;JMP
